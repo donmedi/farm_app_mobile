4dbc83463dc05ec76460ab6bf76de53b6cc88fab
Halimah-Usman <halimaasabe37@gmail.com>
2024-09-15 13:20:58 +0100

    fix

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 class Constant {
-  static String apiMainUrl = 'http://localhost:4000';
+  static String apiMainUrl = 'http://10.0.2.2:4000';
   //  'http://localhost:4000';
   // Platform.version == 'ios'
   //     ? 'http://localhost:4000'
